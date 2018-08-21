548b9825ecea8ed4905d81b5a98b3b2fd3692dda
SHASHANK-ABACUS\shashank-abacus <shashank@abacusdesk.co.in>
2018-08-21 20:51:42 +0530

    materialize added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import "materialize-css";
+import { MaterializeModule } from 'angular2-materialize';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,6 +16,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    MaterializeModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {MaterializeAction} from 'angular2-materialize';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,15 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  modalActions = new EventEmitter<string|MaterializeAction>();
+  openModal() {
+    this.modalActions.emit({action:"modal",params:['open']});
+  }
+  
+  closeModal() {
+    this.modalActions.emit({action:"modal",params:['close']});
   }
 
 }
